a3c3dbe97d2307d29165f7974d80f77b71f15e60
mikastiv <mikastiv@outlook.com>
2022-04-18 20:01:52 -0400

    map typedefs

--- map.hpp
+++ map.hpp
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 12:17:41 by mleblanc          #+#    #+#             */
-/*   Updated: 2022/04/18 12:31:45 by mleblanc         ###   ########.fr       */
+/*   Updated: 2022/04/18 19:57:54 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,23 @@
 
 #include "utility.hpp"
 
-template<
-    typename Key,
-    typename T,
-    typename Compare = ft::less<Key>,
-    typename Allocator = std::allocator<ft::pair<const Key, T> >
-> class map{};
+namespace ft
+{
+template <typename Key, typename T, typename Compare = less<Key>,
+          typename Allocator = std::allocator<pair<const Key, T> > >
+class map
+{
+  public:
+    typedef Key key_type;
+    typedef T mapped_type;
+    typedef pair<const key_type, mapped_type> value_type;
+    typedef Compare key_compare;
+    typedef Allocator allocator_type;
+    typedef typename allocator_type::size_type size_type;
+    typedef typename allocator_type::difference_type difference_type;
+    typedef value_type& reference;
+    typedef const value_type& const_reference;
+    typedef typename allocator_type::pointer pointer;
+    typedef typename allocator_type::const_pointer const_pointer;
+};
+} // namespace ft
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 15:27:15 by mleblanc          #+#    #+#             */
-/*   Updated: 2022/04/17 17:55:54 by mleblanc         ###   ########.fr       */
+/*   Updated: 2022/04/18 19:55:13 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,16 @@
 
 namespace ft
 {
-template <typename T, typename Alloc = std::allocator<T> >
+template <typename T, typename Allocator = std::allocator<T> >
 class vector
 {
   public:
     typedef T value_type;
-    typedef Alloc allocator_type;
+    typedef Allocator allocator_type;
     typedef typename allocator_type::size_type size_type;
     typedef typename allocator_type::difference_type difference_type;
-    typedef typename allocator_type::reference reference;
-    typedef typename allocator_type::const_reference const_reference;
+    typedef value_type& reference;
+    typedef const value_type& const_reference;
     typedef typename allocator_type::pointer pointer;
     typedef typename allocator_type::const_pointer const_pointer;
     typedef normal_iterator<pointer, vector> iterator;
