14c1ad1ae8420f43eb90485bc7a175a3f256e9b1
mleblanc <mleblanc@student.42quebec.com>
2022-04-20 17:52:26 -0400

    prototypes

--- set.hpp
+++ set.hpp
@@ -1,55 +1,50 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   map.hpp                                            :+:      :+:    :+:   */
+/*   set.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/18 12:17:41 by mleblanc          #+#    #+#             */
-/*   Updated: 2022/04/20 17:52:15 by mleblanc         ###   ########.fr       */
+/*   Created: 2022/04/20 16:29:45 by mleblanc          #+#    #+#             */
+/*   Updated: 2022/04/20 17:51:38 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <memory>
-
 #include "utility.hpp"
+#include <memory>
 
 namespace ft
 {
-template <typename Key, typename T, typename Compare = less<Key>,
-          typename Allocator = std::allocator<pair<const Key, T> > >
-class map
+template <typename Key, typename Compare = ft::less<Key>, typename Allocator = std::allocator<Key> >
+class set
 {
   public:
     typedef Key key_type;
-    typedef T mapped_type;
-    typedef pair<const key_type, mapped_type> value_type;
+    typedef Key value_type;
     typedef Compare key_compare;
+    typedef Compare value_compare;
     typedef Allocator allocator_type;
-    typedef typename allocator_type::size_type size_type;
     typedef typename allocator_type::difference_type difference_type;
+    typedef typename allocator_type::size_type size_type;
     typedef value_type& reference;
     typedef const value_type& const_reference;
     typedef typename allocator_type::pointer pointer;
     typedef typename allocator_type::const_pointer const_pointer;
 
   public:
-    map();
-    explicit map(const key_compare& comp, const allocator_type& alloc = allocator_type());
-    template <typename InputIt>
-    map(InputIt first, InputIt last, const key_compare& comp = key_compare(),
+    set();
+    explicit set(const key_compare& comp, const allocator_type& alloc = allocator_type());
+    template <class InputIt>
+    set(InputIt first, InputIt last, const key_compare& comp = key_compare(),
         const allocator_type& alloc = allocator_type());
-    map(const map& other);
-    map& operator=(const map& other);
-    ~map();
+    set(const set& other);
+    set& operator=(const set& other);
+    ~set();
 
   public:
     allocator_type get_allocator() const;
-    reference at(const key_type& key);
-    const_reference at(const key_type& key) const;
-    reference operator[](const key_type& key);
     iterator begin();
     const_iterator begin() const;
     iterator end();
@@ -69,7 +64,7 @@ class map
     void erase(iterator pos);
     void erase(iterator first, iterator last);
     size_type erase(const key_type& key);
-    void swap(map& other);
+    void swap(set& other);
     size_type count(const key_type& key) const;
     iterator find(const key_type& key);
     const_iterator find(const key_type& key) const;
@@ -81,10 +76,11 @@ class map
     const_iterator upper_bound(const key_type& key) const;
     key_compare key_comp() const;
     // value_compare value_comp() const;
+
 };
 
-template <typename Key, typename T, typename Compare, typename Allocator>
-inline void swap(map<Key, T, Compare, Allocator>& lhs, map<Key, T, Compare, Allocator>& rhs)
+template <typename Key, typename Compare, typename Allocator>
+void swap(set<Key, Compare, Allocator>& lhs, set<Key, Compare, Allocator>& rhs)
 {
     lhs.swap(rhs);
 }
