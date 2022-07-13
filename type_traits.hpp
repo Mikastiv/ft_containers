4647668adfdd81e3a3e038d83ccc3059d95e73c4
mleblanc <mleblanc@student.42quebec.com>
2022-07-13 18:13:05 -0400

    fix is_integral (remove_cv)

--- type_traits.hpp
+++ type_traits.hpp
@@ -34,48 +34,77 @@ struct false_type {
     typedef false_type type;
 };
 
+template <typename T>
+struct remove_const {
+    typedef T type;
+};
+
+template <typename T>
+struct remove_const<const T> {
+    typedef T type;
+};
+
+template <typename T>
+struct remove_volatile {
+    typedef T type;
+};
+
+template <typename T>
+struct remove_volatile<volatile T> {
+    typedef T type;
+};
+
+template <typename T>
+struct remove_cv {
+    typedef typename remove_volatile<typename remove_const<T>::type>::type type;
+};
+
 template <typename>
-struct is_integral : public false_type {
+struct is_integral_base : public false_type {
 };
 
 template <>
-struct is_integral<bool> : public true_type {
+struct is_integral_base<bool> : public true_type {
 };
 
 template <>
-struct is_integral<char> : public true_type {
+struct is_integral_base<char> : public true_type {
 };
 
 template <>
-struct is_integral<signed char> : public true_type {
+struct is_integral_base<signed char> : public true_type {
 };
 
 template <>
-struct is_integral<short int> : public true_type {
+struct is_integral_base<short int> : public true_type {
 };
 
 template <>
-struct is_integral<int> : public true_type {
+struct is_integral_base<int> : public true_type {
 };
 
 template <>
-struct is_integral<long int> : public true_type {
+struct is_integral_base<long int> : public true_type {
 };
 
 template <>
-struct is_integral<unsigned char> : public true_type {
+struct is_integral_base<unsigned char> : public true_type {
 };
 
 template <>
-struct is_integral<unsigned short int> : public true_type {
+struct is_integral_base<unsigned short int> : public true_type {
 };
 
 template <>
-struct is_integral<unsigned int> : public true_type {
+struct is_integral_base<unsigned int> : public true_type {
 };
 
 template <>
-struct is_integral<unsigned long int> : public true_type {
+struct is_integral_base<unsigned long int> : public true_type {
+};
+
+template <typename T>
+struct is_integral : is_integral_base<typename remove_cv<T>::type> {
 };
 
 template <typename T, typename U>
